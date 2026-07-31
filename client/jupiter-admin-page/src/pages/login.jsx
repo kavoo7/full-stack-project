f2b25039c0f0feb9a9d9707891107cb5274fe8de
Conelius Kavoo <kavoocornelius7@gmail.com>
2026-07-31 11:00:23 +0300

    feat: implement login functionality with JWT authentication

--- client/jupiter-admin-page/src/pages/login.jsx
+++ client/jupiter-admin-page/src/pages/login.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/api";
+
 function Login() {
-    return <h1>Login</h1>;
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", response.data.access_token);
+      console.log("Token:", localStorage.getItem("token"));
+
+      alert("Login successful!");
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Invalid email or password");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Jupiter School Admin Login</h1>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
